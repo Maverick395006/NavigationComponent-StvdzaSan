891ec7f3b573feb59f10af258a590a99e4da2e64
Maverick395006 <87902602+Maverick395006@users.noreply.github.com>
2022-12-25 14:09:56 +0530

    1. Nav Graph Added    FragmentContainer Added    Two Fragment Added    One Fragment to another fragment move Added    PopUp Added    Animation Added

---
+++ app/src/main/java/com/maverick/navigationcomponent_stevdzasan/SecondFragment.kt
@@ -0,0 +1,42 @@
+package com.maverick.navigationcomponent_stevdzasan
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.maverick.navigationcomponent_stevdzasan.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding =FragmentSecondBinding.inflate(inflater,container,false)
+
+
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvFragmentTwo.setOnClickListener{
+            findNavController().navigate(R.id.action_secondFragment_to_firstFragment)
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
